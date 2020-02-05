aff5bc13a6b8c82028a4fa16cea80c6ba3ca22ce
iyasmzn <iyasmzn07@gmail.com>
2020-02-05 14:19:29 +0700

    add work query auto count

--- assets/script.js
+++ assets/script.js
@@ -21,16 +21,44 @@ $(document).ready(function() {
 			});
 			$('.carousel').carousel();
 		});
-			$(window).scroll(function() {
-				var navbar = $('.nava').offset().top;
-				var scroll = $(window).scrollTop();
-				if (scroll > 500 ) {
-					$('.nava').addClass('sticky-header').removeClass('nava-abs').removeClass('py-4');
-					$('.fixed-upper').css({"bottom":"20px"});
-					$('.nava').animate({top:"0px",opacity:"1"},'slow');
-				}
-				else {
-					$('.nava').removeClass('sticky-header').addClass('nava-abs').addClass('py-4');	
-					$('.fixed-upper').css({"bottom":"-200px"});
-				}
-			});
+		var a = 0;
+		$(window).scroll(function() {
+			// NAVBAR
+			var navbar = $('.nava').offset().top,
+					scroll = $(window).scrollTop();
+			if (scroll > 500 ) {
+				$('.nava').addClass('sticky-header').removeClass('nava-abs').removeClass('py-4');
+				$('.fixed-upper').css({"bottom":"20px"});
+				$('.nava').animate({top:"0px",opacity:"1"},'slow');
+			}
+			else {
+				$('.nava').removeClass('sticky-header').addClass('nava-abs').addClass('py-4');	
+				$('.fixed-upper').css({"bottom":"-200px"});
+			}
+
+			// WORK COUNT 
+			var cTop = $('#count').offset().top - window.innerHeight;
+			if (a == 0 && scroll > cTop ) {
+				$('.counter-value').each(function() {
+					var $this 	= $(this),
+							countTo = $this.attr('data-count');
+					$({
+						countNum: $this.text()
+					}).animate({
+						countNum: countTo
+					},
+					{
+						duration: 5*1000,
+						easing: 'swing',
+						step: function() {
+							$this.text(Math.floor(this.countNum));
+						},
+						complete: function() {
+							$this.text(this.countNum);
+							// alert('finished');
+						}
+					});
+				});
+				a = 1;
+			}
+		});
